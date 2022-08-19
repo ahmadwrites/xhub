55676d7f3d61700165b2b3657decd5e6f9377a8d
worknovel <ahmadhasnol@hotmail.com>
2022-08-19 22:57:47 +0800

    Added user auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,1 +1,49 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import { createError } from "../error.js";
+
+export const signup = async (req, res, next) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    const newUser = new User({ ...req.body, password: hashedPassword });
+
+    await newUser.save();
+    res.status(200).json("Successfully created user.");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createError(404, "User does not exist!"));
+
+    const verifyPassword = bcrypt.compareSync(req.body.password, user.password);
+    if (!verifyPassword) return next(createError(400, "Invalid credentials!"));
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_KEY);
+    const { password, ...others } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signout = (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("Successfully logged out.");
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { signin, signout, signup } from "../controllers/authController.js";
 
 const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.post("/signout", signout);
 
 export default router;
